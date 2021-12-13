8311a01b56ad547bfc19198d026b3bc60caca8ba
Ghaida50 <81191898+Ghaida50@users.noreply.github.com>
2021-12-13 15:38:56 +0300

    Update signin.java

--- signin.java
+++ signin.java
@@ -25,6 +25,9 @@ Connection connection = null;
     
     public signin() {
         initComponents();
+	Toolkit tk = Toolkit.getDefaultToolkit();
+        Image   titleIcon = tk.getImage(getClass().getResource("rs.jfif"));
+        setIconImage(titleIcon);
          doConnect();
     }
      public void doConnect( ) {
