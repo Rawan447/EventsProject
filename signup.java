9327d5111696e6da25e265c6478ae88087751adf
Ghaida50 <81191898+Ghaida50@users.noreply.github.com>
2021-12-13 15:40:06 +0300

    Update signup.java

--- signup.java
+++ signup.java
@@ -24,6 +24,9 @@ public class signup extends javax.swing.JFrame {
     String query;
     public signup() {
         initComponents();
+	Toolkit tk = Toolkit.getDefaultToolkit();
+        Image   titleIcon = tk.getImage(getClass().getResource("rs.jfif"));
+        setIconImage(titleIcon);
          doConnect();
     }
      public void doConnect( )
